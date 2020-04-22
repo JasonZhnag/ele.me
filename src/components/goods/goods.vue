3f5e82ff70da136d5f5fec5549ebaa3be6a74811
jasonzhnag <zhangbeichen819@163.com>
2020-04-22 20:05:48 +0800

    Begin Goods component

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>
-    I am seller
+  <div class="goods">
+    <div class="scroll-nav-wrapper">
+
+    </div>
+    <div class="shop-cart-wrapper">
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'seller'
+  name: 'goods'
 }
 </script>
+
+<style scoped lang="stylus">
+@import '~common/stylus/mixin'
+@import '~common/stylus/variable'
+
+.goods
+  position relative
+</style>
